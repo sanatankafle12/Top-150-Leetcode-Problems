1500703ad7edd2ea941abdd19fabbdd54fd7a174
shrigsanatan <sanatan@shrigsolutions.com>
2025-02-16 19:09:44 +0545

    Made changes to the Ransom Note Problem, i.e, solved using HashMap

--- Hashmap/ransom_note.py
+++ Hashmap/ransom_note.py
@@ -1,13 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom_list = list(ransomNote)
-        magazine_list = list(magazine)
-        copy_list = list(ransomNote)
-        for x in copy_list:
-            if x in magazine_list:
-                ransom_list.remove(x)
-                magazine_list.remove(x)
+        dictionary = {}
 
-        if len(ransom_list) == 0:
-            return True
-        return False
+        for elements in magazine:
+            if elements not in dictionary:
+                dictionary[elements] =  1
+            else:
+                dictionary[elements] += 1
+
+        for elements in ransomNote:
+            if elements in dictionary and dictionary[elements]>0:
+                dict[elements] -=1
+            else:
+                return False
+        
+        return True
